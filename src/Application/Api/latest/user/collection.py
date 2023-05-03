1e05d54820a567af81744dc95d08ee32b1c2f94d
malkmaneth <miguelflores.preface@gmail.com>
2023-05-03 14:13:47 +0800

    refactor: user subdir

--- src/Application/Api/latest/user/collection.py
+++ src/Application/Api/latest/user/collection.py
@@ -1,7 +1,6 @@
 from Extensions.Nestable import NestableBlueprint
 
-
-
 __author__ = 'KycKyc'
-collection = NestableBlueprint("Application", __name__)
+
+collection = NestableBlueprint('User', __name__, url_prefix='/user')
 collection.register_collection(__file__)
