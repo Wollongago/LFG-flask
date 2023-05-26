16132aadb90deb05a6bc3866120c123ac0b16554
ronjoshmap <ronjoshmap@hotmail.com>
2023-05-26 22:25:29 +0800

    chore: pushing previously undone code

--- src/Celery/tasks/steam.py
+++ src/Celery/tasks/steam.py
@@ -1,23 +1,39 @@
-from Extensions.Parsers.steam_parse import SteamParser
-from Application.Api.latest.__schemas import user
-#import requests
+# from Extensions.Parsers.steam_parse import SteamParser
+# from Application.Api.latest.user.views import *
+import sys
+# from Extensions import celery_app
+from flask import Flask
 from Extensions import flask_pymongo
-from Extensions import celery_app
-
-@celery_app.task
-def update_user_schema():
-    profile = get_user_details()
-    user.fields.game_account = profile('steamid')
-
-@celery_app.task
-def sync_user_profile(steam_id):
-    with celery_app.app_context():
-        # TODO: Add code to sync the user's profile to your application's database
-        for users in flask_pymongo.db.users.find():
-            flask_pymongo.save_file(users)
-        
 
 
+# Testing purposes, since can't import SteamParser (import issues)
+user_name = "ronjosh"
+steam_id = "76561197960520747"
+print (user_name, steam_id)
 
 
-
+# @celery_app.task
+# @app.route("/")
+# def sync_user_profile(user_name, steam_id):
+def sync_user_profile():
+#     # TODO: Add code to sync the user's profile to your application's database
+#     parser = SteamParser()
+#     steam_id = "76561197960520747" #testing purpose
+#     UserSteamId = parser.get_user_details(steam_id)
+    # users = flask_pymongo.db.test.find({"$or": [{"game_id": { "$exists": "false" }}, { "game_id": "null" }]})
+    flask_pymongo.db.test.insertOne({"username": "ronjosh", "game_id": "null"})
+    users = flask_pymongo.db.test.find()
+    return users
+    # for user in users:
+        # if(user.username == user_name): #not sure if this line is correct
+        # # if(flask_pymongo.db.test.find_or_404({"username": "user_name"})):
+        #     user.game_id = steam_id #UserSteamId('steamid') 
+        #     flask_pymongo.db.test.updateOne({"username": user_name}, {"$set": {"game_id": steam_id}}) #variable could be wrong
+        #     flask_pymongo.save_file()                                          
+        # return 'Database synchronized successfully!' 
+    
+# sync_user_profile("ronjosh", "76561197960520747")
+sync_user_profile()
+modulename = 'flask_pymongo'
+if modulename not in sys.modules:
+    print ('You have not imported the {} module'.format(modulename))
--- src/Extensions/Parsers/steam_parse.py
+++ src/Extensions/Parsers/steam_parse.py
@@ -1,14 +1,15 @@
 from steam import Steam
+#from src.credentials import STEAM_API_KEY
 
-from src.credentials import STEAM_API_KEY
-
-#import os
-
-steam = Steam(STEAM_API_KEY)
+steam = Steam('STEAM_API_KEY')
 
 class SteamParser():
-    def get_user_details():
-        user = steam.users.get_user_details("<steam_id>")
+    def get_user_details(steam_id):
+        user = steam.users.get_user_details(steam_id)
+        steamid_value = user.get("player", "not found").get("steamid", "not found")
+        username_value = user.get("player", "not found").get("personaname", "not found")
+        print (steamid_value, username_value)
+        return (steamid_value, username_value)
 
     def get_game_details():
         apex_app_id = 1172470
@@ -25,7 +26,6 @@ class SteamParser():
         user = steam.apps.get_user_game_achievements("<steam_id>", "<app_id>")
 
 # testing output
-if __name__ == "__main__":
-    #print(os.getcwd)
-    user = get_user_details("76561197960520747")
-    print(user)
+# if __name__ == "__main__":
+#     user = SteamParser.get_user_details("76561197960520747")
+
