b52a36e236bc804b0e1a1910285a27f81d66fbe8
ronjoshmap <ronjoshmap@hotmail.com>
2023-05-28 21:22:55 +0800

    chore: attempt to change line endings to LF fix: fixed parser, using constructor and self now fix: data sync (working when using endpoint to test)

--- src/Celery/tasks/steam.py
+++ src/Celery/tasks/steam.py
@@ -1,39 +1,18 @@
 # from Extensions.Parsers.steam_parse import SteamParser
-# from Application.Api.latest.user.views import *
-import sys
 # from Extensions import celery_app
-from flask import Flask
 from Extensions import flask_pymongo
 
-
 # Testing purposes, since can't import SteamParser (import issues)
-user_name = "ronjosh"
+user_name = "test"
 steam_id = "76561197960520747"
-print (user_name, steam_id)
 
 
 # @celery_app.task
-# @app.route("/")
-# def sync_user_profile(user_name, steam_id):
 def sync_user_profile():
 #     # TODO: Add code to sync the user's profile to your application's database
 #     parser = SteamParser()
-#     steam_id = "76561197960520747" #testing purpose
-#     UserSteamId = parser.get_user_details(steam_id)
-    # users = flask_pymongo.db.test.find({"$or": [{"game_id": { "$exists": "false" }}, { "game_id": "null" }]})
-    flask_pymongo.db.test.insertOne({"username": "ronjosh", "game_id": "null"})
-    users = flask_pymongo.db.test.find()
-    return users
-    # for user in users:
-        # if(user.username == user_name): #not sure if this line is correct
-        # # if(flask_pymongo.db.test.find_or_404({"username": "user_name"})):
-        #     user.game_id = steam_id #UserSteamId('steamid') 
-        #     flask_pymongo.db.test.updateOne({"username": user_name}, {"$set": {"game_id": steam_id}}) #variable could be wrong
-        #     flask_pymongo.save_file()                                          
-        # return 'Database synchronized successfully!' 
-    
-# sync_user_profile("ronjosh", "76561197960520747")
-sync_user_profile()
-modulename = 'flask_pymongo'
-if modulename not in sys.modules:
-    print ('You have not imported the {} module'.format(modulename))
+    queryall = flask_pymongo.db.test.find()
+    for user in queryall:
+        if(flask_pymongo.db.test.find({"$or": [{"game_id": { "$exists": "false" }}, { "game_id": "null" }]})):
+            flask_pymongo.db.test.update({"username": user_name}, {"$set": {"game_id": steam_id}})  
+    return('Data sync success!')
--- src/Extensions/Parsers/steam_parse.py
+++ src/Extensions/Parsers/steam_parse.py
@@ -4,28 +4,19 @@ from steam import Steam
 steam = Steam('STEAM_API_KEY')
 
 class SteamParser():
-    def get_user_details(steam_id):
-        user = steam.users.get_user_details(steam_id)
-        steamid_value = user.get("player", "not found").get("steamid", "not found")
-        username_value = user.get("player", "not found").get("personaname", "not found")
-        print (steamid_value, username_value)
-        return (steamid_value, username_value)
+    def __init__(self, steam_id):
+        self.steam_id = steam_id
+        print('Constructor called!')
 
-    def get_game_details():
-        apex_app_id = 1172470
-
-        # arguments: app_id
-        user = steam.apps.get_game_details(apex_app_id)
-
-    def get_user_game_details():
-        # arguments: steam_id, app_id
-        user = steam.apps.get_user_game_details("<steam_id>", "<app_id>")
-
-    def get_user_game_achievements():
-        # arguments: steam_id, app_id
-        user = steam.apps.get_user_game_achievements("<steam_id>", "<app_id>")
+    def get_user_details(self, steam_id):
+        self.user = steam.users.get_user_details(steam_id)
+        self.steamid_value = self.user.get("player", "not found").get("steamid", "not found")
+        self.username_value = self.user.get("player", "not found").get("personaname", "not found")
+        print (self.steamid_value, self.username_value)
+        return (self.steamid_value, self.username_value)
 
 # testing output
-# if __name__ == "__main__":
-#     user = SteamParser.get_user_details("76561197960520747")
+if __name__ == "__main__":
+    parser = SteamParser("76561197960520747")
+    user = parser.get_user_details("76561197960520747")
 
