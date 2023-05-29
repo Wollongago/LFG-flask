5809df2db50c820d18a869a1c424aca55d52c6ab
lonnstyle <lonnstyle@gmail.com>
2023-05-29 14:31:59 +0800

    fix: Celery scheduling for steam parser

--- src/Celery/celery_scheduler.py
+++ src/Celery/celery_scheduler.py
@@ -3,14 +3,18 @@ import os
 from celery import Celery
 from Celery.celery_worker import celery_app
 from celery.schedules import crontab
-from Celery.tasks.steam import sync_user_profile
+from Celery.tasks import steam
 
+# Periodic-synchronization for steam user-profiles
+celery_app.add_periodic_task(crontab(minute="0",hour="*"),
+                             steam.sync_user_profile.s(),
+                             expires=60*5)
 
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     print('setup_periodic_tasks')
     print(sender)
 
-# Periodic-synchronization for steam user-profiles
-celery_app.add_periodic_task(crontab(minute=0, hour=0), sync_user_profile())
+
+
 
--- src/Celery/tasks/steam.py
+++ src/Celery/tasks/steam.py
@@ -1,18 +1,18 @@
 # from Extensions.Parsers.steam_parse import SteamParser
-# from Extensions import celery_app
-from Extensions import flask_pymongo
+from Extensions import celery_app, flask_pymongo
 
 # Testing purposes, since can't import SteamParser (import issues)
 user_name = "test"
 steam_id = "76561197960520747"
 
 
-# @celery_app.task
+@celery_app.task(name="steam.sync")
 def sync_user_profile():
+    print("sync_user_profile")
 #     # TODO: Add code to sync the user's profile to your application's database
-#     parser = SteamParser()
-    queryall = flask_pymongo.db.test.find()
-    for user in queryall:
-        if(flask_pymongo.db.test.find({"$or": [{"game_id": { "$exists": "false" }}, { "game_id": "null" }]})):
-            flask_pymongo.db.test.update({"username": user_name}, {"$set": {"game_id": steam_id}})  
-    return('Data sync success!')
+    # parser = SteamParser()
+    # queryall = flask_pymongo.db.test.find()
+    # for user in queryall:
+    #     if(flask_pymongo.db.test.find({"$or": [{"game_id": { "$exists": "false" }}, { "game_id": "null" }]})):
+    #         flask_pymongo.db.test.update({"username": user_name}, {"$set": {"game_id": steam_id}})  
+    # return('Data sync success!')
