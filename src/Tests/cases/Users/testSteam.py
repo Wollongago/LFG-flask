a0c6774d72447ba766f278b6de0ee6e5c031cf0c
ronjoshmap <ronjoshmap@hotmail.com>
2023-06-26 15:19:02 +0800

    added src/Tests/cases/Users/testSteam.py

---
+++ src/Tests/cases/Users/testSteam.py
@@ -0,0 +1,34 @@
+import unittest
+from Celery.tasks.steam import SteamParser
+from Tests.cases.base import BaseTest
+
+class Test(BaseTest):
+  
+  def test_steam(self):
+    parser = SteamParser()
+    get_steam_id = str(76561198034973737) # Testing case
+
+    parser.update_parser(get_steam_id)
+    parse_user_details = parser.get_user_details()
+    parse_user_games = parser.get_user_games()
+    parse_user_app_achievements = parser.get_user_app_achievements()
+
+    testDocument = {
+       "game_id": get_steam_id,
+       "username": parse_user_details[0],
+       "profile_url": parse_user_details[1],
+       "country": parse_user_details[2],
+       "games_owned": parse_user_games,
+       "apex_achievements": parse_user_app_achievements
+    }
+
+    self.assertEqual(testDocument["game_id"], get_steam_id)
+    self.assertEqual(testDocument["username"], parse_user_details[0])
+    self.assertEqual(testDocument["profile_url"], parse_user_details[1])
+    self.assertEqual(testDocument["country"], parse_user_details[2])
+    self.assertEqual(testDocument["games_owned"], parse_user_games)
+    self.assertEqual(testDocument["apex_achievements"], parse_user_app_achievements)
+
+
+if __name__ == "__main__":
+    unittest.main()
