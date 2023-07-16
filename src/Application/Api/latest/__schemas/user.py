34cec906a1424b3766d796ea1b5718ac19319025
lonnstyle <54892447+lonnstyle@users.noreply.github.com>
2023-07-16 19:43:44 +0800

    feat: user profile endpoint (#22)

--- src/Application/Api/latest/__schemas/user.py
+++ src/Application/Api/latest/__schemas/user.py
@@ -1,14 +1,12 @@
+from bson import ObjectId
 from Extensions import flask_pymongo
 from marshmallow import Schema, ValidationError, fields, post_load, pre_load, validate, validates, validates_schema
-from bson import ObjectId
+
 
 class User:
     fields = [
         '_id',
-        'username',
-        'password',
-        'game_account',
-        'avatar',
+        'steam_profile'
     ]
 
     def dump(self,data):
@@ -19,28 +17,12 @@ class User:
         return [self.dump(data) for data in data_list]
 class UserSchema(Schema):
     _id = fields.Str(dump_only=True)
-    username = fields.Str(required=True, validate=validate.Length(min=1, max=50))
-    password = fields.Str(required=True, validate=validate.Length(min=8, max=50))
-    email = fields.Email(required=True)
-    game_account = fields.Str(validate=validate.Length(max=50))
-    avatar = fields.Str(validate=validate.Length(max=100))
+    steam_profile = fields.Dict()
 
-    # @validates('user')
-    # def validate_user(self, value):
-    #     if not flask_pymongo.db.users.find_one({'_id': ObjectId(value)}):
-    #         raise ValidationError('User does not exist', 'user')
-    
-    # check if valid username
-    @validates_schema
-    def validate_username(self, data):
-        if flask_pymongo.db.users.find_one({'username': data['username']}):
-            raise ValidationError('Username already exists', 'username')
-        
-    # check if valid email
-    # @validates_schema
-    # def validate_email(self, data):
-    #     if not flask_pymongo.db.users.find_one({'email': data['email']}):
-    #         raise ValidationError('Email does not exist', 'email')
+    @validates('user')
+    def validate_user(self, value):
+        if not flask_pymongo.db.users.find_one({'_id': ObjectId(value)}):
+            raise ValidationError('User does not exist', 'user')
     
     
     @post_load
